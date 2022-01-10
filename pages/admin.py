f7b3a92097f69f6b9c549687d29139fdb585fd5b
Titas Das <dastitas20@gmail.com>
2022-01-11 03:30:31 +0530

    Few Changes

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import team
+from django.utils.html import format_html
 # Register your models here.
-admin.site.register(team)
+
+class teamadmin(admin.ModelAdmin):
+        def thumbnail(self,object):
+            return format_html('<img src="{}" width="100" style="border-radius: 50px" />'.format(object.photo.url))
+
+        list_display = ('id','first_name','thumbnail','designation','created_date')
+        list_display_links = ('id','thumbnail','first_name')
+        search_fields = ('first_name','last_name','designation')
+        list_filter = ('designation',)
+
+admin.site.register(team, teamadmin)
--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,7 @@ class team(models.Model):
     last_name = models.CharField(max_length=255)
     designation = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    fb_link=models.URLField(max_length=100)
+    facebook_link=models.URLField(max_length=100)
     twitter_link=models.URLField(max_length=100)
     google_plus_link=models.URLField(max_length=100)
     created_date=models.DateTimeField(auto_now_add=True)
